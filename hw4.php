5921b6452735feb08008894cd4416bc97a9ca209
Ian Lowery <ianrl@udel.edu>
2012-10-05 18:51:46 -0400

    first commit

---
+++ hw4.php
@@ -0,0 +1,19 @@
+<?php
+print("My favorite color is gray");
+
+print "<br>";
+$DaysPerWeek=(7);
+$HoursPerDay=(24);
+$MinutesPerHour=(60);
+$MinutesPerWeek=$DaysPerWeek*$HoursPerDay*$MinutesPerHour;
+
+print ($MinutesPerWeek);
+
+print "<br>";
+
+$HoursPerDay=(26);
+$MinutesPerWeek=$DaysPerWeek*$HoursPerDay*$MinutesPerHour;
+print ($MinutesPerWeek);
+
+
+_?>
